b63e9bce33d67ab8364e3ce4479ccae772904f21
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-07-01 14:16:01 -0400

    added reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose')
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+            ref: 'User'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+
+const Reaction = model('reaction', reactionSchema)
+
+module.exports = Reaction
