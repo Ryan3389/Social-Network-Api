94cd520482a6cdd9b4797306287d852685f2e643
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-07-01 14:29:52 -0400

    updated user, thought and reaction model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const reactionSchema = require('../models/Reaction')
 
 const thoughtSchema = new Schema(
     {
@@ -10,12 +11,19 @@ const thoughtSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            get: timestamp => new Date(timestamp).toLocaleString()
         },
         username: {
             type: String,
             required: true,
             ref: 'User'
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
         }
     }
 )
