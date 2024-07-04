f40c9d26ceb7d12e5a0a348841f90aa9b142e233
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-07-04 15:16:10 -0400

    added thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thoughtControllers')
+
+module.exports = router
