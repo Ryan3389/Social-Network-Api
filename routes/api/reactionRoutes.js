9351815ce1f4a83765fb5448d7ecf434784e8e5c
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-07-04 22:40:20 -0400

    fixed bugs in routes

--- routes/api/reactionRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const {
-    createReaction,
-    deleteReaction
-} = require('../../controllers/reactionControllers')
-
-router.route('/').post(createReaction)
-router.route('/:id').delete(deleteReaction)
